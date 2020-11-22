ff70418479f4151ffe55f70d6a6b473fd256156f
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-11-22 17:15:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,11 +3,6 @@
 //1.
 // Write findMatching- This function takes an array of drivers and a string as arguments,
 //and returns the matching list of drivers. The function should be case insensitive.
-findMatching = drivers => {
-  let result = drivers.filter(name => name.result === name)
-  return !!result ? result : []
-}
-
 function findMatching (list, name) {
   return list.filter(function (driverName) {
     return driverName.toLowerCase() === name.toLowerCase();
