58860950c306971e2ee114f1bc8a1da5d6d44c2f
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-11-22 17:23:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,7 +24,9 @@ function matchName (drivers, name) {
 //Write fuzzyMatch - This function takes an array of drivers and a string as arguments
 //for querying the array, and returns all drivers whose names begin with the provided
 //letters.
-function fuzzyMatch(drivers, letters) {}
+function fuzzyMatch(drivers, letters) {
+  let length = letters.length;
+}
 
 // function fuzzyMatch (list, partialName) {
 //   let lengthOfName = partialName.length;
